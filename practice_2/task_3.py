3a8731dc5952568da947a95b94ddd4395219f588
fopdf <rogov.rd@phystech.edu>
2024-09-28 21:51:40 +0300

    first commit

--- practice_2/task_3.py
+++ practice_2/task_3.py
@@ -6,6 +6,13 @@ BMI = weight/height**2
 weight = input()
 height = input()
 
-
 #code here
 
+weight_float = float(weight)
+height_float = float(height)
+
+if (weight_float > 0) and (height_float > 0):
+    BMI = weight_float/height_float**2
+    print(f'Your BMI: {round(BMI,2)}') 
+else:
+    print('Enter valid height and weight')
--- practice_2/task_4.py
+++ practice_2/task_4.py
@@ -4,3 +4,19 @@ Develop a program that asks the user for an integer 4-digit number and calculate
 3 + 1 + 4 + 1 = 9
 
 """
+
+print('Enter 4-digit number')
+num = input()
+
+try:
+    num_int = int(num)
+
+    if ((num_int > 999) and (num_int < 10000)):
+        list_digit = [int(i) for i in str(num_int)]
+        sum = list_digit[0]+list_digit[1]+list_digit[2]+list_digit[3]  
+        print(f'Sum of digits: {sum}')
+    else: 
+        print('Enter valid number')
+
+except ValueError:
+    print('Enter valid number')
